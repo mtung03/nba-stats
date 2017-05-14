359e751915738b2c1a4d51c9daddeadd2f9e5017
Maxwell Tung <max.tung.3@gmail.com>
2017-05-14 13:54:31 -0400

    updated js

--- nba.js
+++ nba.js
@@ -1,130 +1,168 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
+var Alexa = require('alexa-sdk');
+var https = require('https');
+var btoa = require('btoa-atob');
 
-const Alexa = require('alexa-sdk');
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+var APP_ID = undefined;
 
-const languageStrings = {
-    'en-GB': {
-        translation: {
-            FACTS: [
-                'Earth is actually larger than the sun',
-                'The mily'
-            ],
-            SKILL_NAME: 'British Space Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    },
-    'en-US': {
-        translation: {
-            FACTS: [
-                'Earth is actually larger than the sun',
-                'The milky way was the third planet to be discovered',
-                'Barack Obama invented the moon',
-                'Earth is flat',
-                'Aliens live on Uranus',
-                'Mars does not exist',
-                'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-                'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-                'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-                'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-                'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-                'Jupiter hat den kürzesten Tag aller Planeten.',
-            ],
-            SKILL_NAME: 'American Space Facts',
-            GET_FACT_MESSAGE: "Didja know? ",
-            TYPE_QUESTION_MESSAGE: "Do you want a real fact or a German fact?",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    },
-    'de-DE': {
-        translation: {
-            FACTS: [
-                'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-                'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-                'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-                'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-                'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-                'Jupiter hat den kürzesten Tag aller Planeten.',
-                'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
-                'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
-                'Die Sonne ist eine fast perfekte Kugel.',
-                'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
-                'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
-                'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
-                'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
-            ],
-            SKILL_NAME: 'Weltraumwissen auf Deutsch',
-            GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-            HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-        },
-    },
+var SKILL_NAME = "Tonight's Games";
+var GET_GAMES_MESSAGE = "The games are ";
+var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_REPROMPT = "What can I help you with?";
+var STOP_MESSAGE = "Goodbye!";
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
+//=========================================================================================================================================
+var Leagues = {
+    "NBA": 'nba',
+    "Basketbal": 'nba',
+    "MLB": 'mlb',
+    "Baseball": 'mlb',
+    "NFL" : 'nfl',
+    "Football": 'nfl',
+    "NHL": 'nhl',
+    "Hockey": 'nhl'
 };
 
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random space fact from the space facts list
-        // Use this.t() to get corresponding language data
-        const factArr = this.t('FACTS');
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
+var formDate = function (d) {
+    return d.replace("-", "");
+};
 
-        // Create speech output
-        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-        //this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
-        this.emit('What type of fact do you want?');
-    },
-    'GetFactType': function () {
-        this.emit('GetType');
-    },
-    'GetType': function () {
-        var userType this.event.request.intent.slots.Type.value;
-        this.emit(userType);
+// format specific to the mysportsfeed api
+var getSeason = function (l, d) {
+    var yr = parseInt(d.slice(0, 5));
+    var md = parseInt(d.slice(5));
+    switch (l) {
+        case 'nba':
+            if (md > parseInt('0630')) {// end of playoffs
+                return toString(yr) + '-' + toString(yr+1) + '-regular';
+            } else if (md < parseInt('0414')) { // start of playoffs
+                return toString(yr-1) + '-' + toString(yr) + '-regular';
+            } else {
+                return toString(yr) + '-playoff';
+            }
+            break;
     }
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = this.t('HELP_MESSAGE');
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
 };
 
-exports.handler = (event, context) => {
-    const alexa = Alexa.handler(event, context);
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.  
+//=========================================================================================================================================
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetGamesIntent');
+    },
+    'GetGamesIntent': function () {
+        var league = "NBA";
+        var date = "2017-05-13";
+        /*if (this.event.request.intent.slots.League.value) {
+            league = this.event.request.intent.slots.League.value;
+        } 
+        if (this.event.request.intent.slots.Date.value) {
+            date = this.event.request.intent.slots.Date.value;
+        }*/
+        var reqLeague = Leagues[league];
+        var reqDate = formDate(date);
+        var result = makeRequest(reqLeague, getSeason(reqLeague, reqDate), reqDate, function() {this.emit('AMAZON.CancelIntent')});
+        
+        var speechOutput = league + " " + date;//"You requested info for " + date + "'s " + league + " games."
+        
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, league);
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
+
+// make an request to the api to return relevant data// request Request 
+var makeRequest = (function(league, season, date, err) {
+    var path = 'https://www.mysportsfeeds.com/api/feed/pull/' + league + '/' + season + '/daily_game_schedule.json?fordate=' + date;
+
+    var options = {
+        hostname: 'www.mysportsfeeds.com',
+        method: 'GET',
+        path: path,
+        headers: {"Authorization":"Basic " + btoa('mtung01:Y3y3pBMY5Ucx')}
+    };
+ 
+    const req = https.request(options, (res) => {
+        console.log(res.statusCode);
+    });
+    req.end();
+    return "ok";
+});
+    /*
+    ('get', path, params, options, function(err, resp) {
+        if (!err && resp.statusCode == 200)
+            return resp.body;  
+    });
+    
+});*/
+/*    'use strict';
+        
+    const httpTransport = require('https');
+    const responseEncoding = 'utf8';
+    const httpOptions = {
+        hostname: 'www.mysportsfeeds.com',
+        port: '443',
+        path: 'https://www.mysportsfeeds.com/api/feed/pull/' + league + '/' + season + '/daily_game_schedule.json?fordate=' + date,
+        method: 'GET',
+        headers: {"Authorization":"Basic " + btoa('mtung01:Y3y3pBMY5Ucx')}
+    };
+    httpOptions.headers['User-Agent'] = 'node ' + process.version;
+ 
+    const request = httpTransport.request(httpOptions, (res) => {
+        let responseBufs = [];
+        let responseStr = '';
+        
+        res.on('data', (chunk) => {
+            if (Buffer.isBuffer(chunk)) {
+                responseBufs.push(chunk);
+            }
+            else {
+                responseStr = responseStr + chunk;            
+            }
+        }).on('end', () => {
+            responseStr = responseBufs.length > 0 ? 
+                Buffer.concat(responseBufs).toString(responseEncoding) : responseStr;
+            
+            err();
+        });
+        
+    })
+    .setTimeout(0)
+    .on('error', (error) => {
+        err();
+    });
+    request.write("")
+    request.end();
+    
+
+})((error, statusCode, headers, body) => {
+    console.log('ERROR:', error); 
+    console.log('STATUS:', statusCode);
+    console.log('HEADERS:', JSON.stringify(headers));
+    console.log('BODY:', body);
+});*/
